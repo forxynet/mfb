344ef2072710ceafadc4f437c5b99f5b6954cb1c
Hakan Baştürk <forxynet@gmail.com>
2023-11-16 16:46:35 +0300

    updated container and marketing

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ signedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Home
           </Typography>
           <Button
             color="primary"
