d2fedd18bf2f38f4b4e38de0a7856226ef6cb6dd
Hakan Baştürk <forxynet@gmail.com>
2023-11-17 11:28:50 +0300

    added and updated auth and container

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -4,8 +4,12 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
-  const history = defaultHistory || createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
 
   if (onNavigate) {
     history.listen(onNavigate);
@@ -13,14 +17,14 @@ const mount = (el, { onNavigate, defaultHistory }) => {
 
   ReactDOM.render(<App history={history} />, el);
 
-  return { 
-    onParentNavigate({pathname: nextPathname}) {
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
       const { pathname } = history.location;
 
-      if(pathname !== nextPathname){
+      if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
-    }
+    },
   };
 };
 
